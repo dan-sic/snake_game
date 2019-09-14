cefe95e7147ad92264267fcd1ebdf950afdace84
Daniel_S <olgierd.dan@gmail.com>
2019-09-14 10:53:35 +0200

    Initial commit

---
+++ build/index.js
@@ -0,0 +1,5 @@
+"use strict";
+console.log("HWDP!");
+setInterval(function () {
+    console.log("HW!");
+}, 1000);
---
+++ index.ts
@@ -0,0 +1,5 @@
+console.log("HWDP!");
+
+setInterval(() => {
+  console.log("HW!");
+}, 1000);
