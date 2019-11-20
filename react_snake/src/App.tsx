35ec783982a790159f49f1d18fed8c32b4ed254f
Daniel_S <olgierd.dan@gmail.com>
2019-11-20 16:46:13 +0100

    Add changing snake direction on keyboard input

--- react_snake/src/App.tsx
+++ react_snake/src/App.tsx
@@ -4,6 +4,7 @@ import { Tile } from "./components/Tile";
 import Grid from "./models/Grid";
 import Snake from "./models/Snake";
 import Cell from "./models/Cell";
+import Direction from "./models/enums/Direction";
 
 class App extends React.Component<{}, { map: Cell[][] }> {
   grid: Grid;
@@ -19,6 +20,7 @@ class App extends React.Component<{}, { map: Cell[][] }> {
 
   componentDidMount() {
     this.initializeSnakeMovement();
+    this.listenToUserKeboardEvents();
   }
 
   initializeSnakeMovement() {
@@ -28,6 +30,28 @@ class App extends React.Component<{}, { map: Cell[][] }> {
       this.snake.move();
       this.setState({ map: this.grid.getMap() });
     }, 150);
+  }
+
+  listenToUserKeboardEvents() {
+    document.addEventListener("keypress", e => {
+      if (e.key === "w" || e.key === "ArrowUp") {
+        this.snake.changeDirection(Direction.UP);
+      }
+
+      if (e.key === "s" || e.key === "ArrowDown") {
+        this.snake.changeDirection(Direction.DOWN);
+      }
+
+      if (e.key === "a" || e.key === "ArrowLeft") {
+        this.snake.changeDirection(Direction.LEFT);
+      }
+
+      if (e.key === "d" || e.key === "ArrowRight") {
+        this.snake.changeDirection(Direction.RIGHT);
+      }
+
+      this.setState({ map: this.grid.getMap() });
+    });
   }
 
   generateTiles() {
