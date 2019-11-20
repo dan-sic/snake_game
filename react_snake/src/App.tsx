53b2cd83a526a46cb545b079f2e0c545101026a5
Daniel_S <olgierd.dan@gmail.com>
2019-11-20 14:02:36 +0100

    Render grid

--- react_snake/src/App.tsx
+++ react_snake/src/App.tsx
@@ -1,26 +1,42 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import { Tile } from "./components/Tile";
+import Grid from "./models/Grid";
+import Snake from "./models/Snake";
+import Cell from "./models/Cell";
 
-const App: React.FC = () => {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  state: { grid: Grid } = {
+    grid: Grid.getInstance(20, 20)
+  };
+
+  componentDidMount() {
+    this.setState({ ...this.state, snake: Snake.getInstance(this.state.grid) });
+  }
+
+  generateTiles() {
+    const gameMap = this.state.grid.getMap();
+
+    const tiles = [];
+
+    for (let i = 0; i < 20; i++) {
+      for (let j = 0; j < 20; j++) {
+        const cell: Cell = gameMap[i][j];
+
+        if (!cell.getSnakeBlock()) {
+          tiles.push(<Tile key={cell.getId()} color='yellow' />);
+        } else {
+          tiles.push(<Tile key={cell.getId()} color='red' />);
+        }
+      }
+    }
+
+    return tiles;
+  }
+
+  render() {
+    return <div className='grid'>{this.generateTiles()}</div>;
+  }
 }
 
 export default App;
