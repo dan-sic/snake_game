1b39974bb5d3e9033468bfed3758d400ef85747f
Daniel_S <olgierd.dan@gmail.com>
2019-11-21 20:32:04 +0100

    Add conditional rendering logic to instructions panel

---
+++ react_snake/src/components/InstructionsPanel.tsx
@@ -0,0 +1,53 @@
+import React from "react";
+import GameState from "../models/enums/GameState";
+
+interface InstructionsPanelProps {
+  gameState: GameState;
+  playerPoints: number;
+}
+
+export const InstructionsPanel: React.SFC<InstructionsPanelProps> = ({
+  gameState,
+  playerPoints
+}) => {
+  switch (gameState) {
+    case GameState.START:
+      return (
+        <div className='instructions-panel'>
+          <div className='instructions'>
+            <p className='highlighted-paragraph'>
+              Press SPACE to start new game
+            </p>
+            <p>Use WSAD keys to navigate snake</p>
+            <p>Use SPACE to pause running game</p>
+          </div>
+        </div>
+      );
+      break;
+    case GameState.PAUSED:
+      return (
+        <div className='instructions-panel'>
+          <div className='instructions'>
+            <p className='highlighted-paragraph'>Game Paused</p>
+            <p>Press SPACE to continue game</p>
+          </div>
+        </div>
+      );
+      break;
+    case GameState.GAME_OVER:
+      return (
+        <div className='instructions-panel'>
+          <div className='instructions'>
+            <p className='highlighted-paragraph'>Game Over</p>
+            <p className='highlighted-paragraph'>
+              You've collected {playerPoints} points
+            </p>
+            <p>Press SPACE to play again</p>
+          </div>
+        </div>
+      );
+      break;
+    default:
+      return null;
+  }
+};
