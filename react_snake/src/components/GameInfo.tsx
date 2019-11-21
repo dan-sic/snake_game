62d842e8fa0fdc6152b2c268a54f8d60acdaa2ff
Daniel_S <olgierd.dan@gmail.com>
2019-11-21 20:31:43 +0100

    Pass player stats down to GameInfo component

--- react_snake/src/components/GameInfo.tsx
+++ react_snake/src/components/GameInfo.tsx
@@ -2,15 +2,23 @@ import React from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faStar, faHeart } from "@fortawesome/free-solid-svg-icons";
 
-export default function GameInfo() {
+interface GameInfoProps {
+  playerPoints: number;
+  playerLives: number;
+}
+
+export const GameInfo: React.SFC<GameInfoProps> = ({
+  playerPoints,
+  playerLives
+}) => {
   return (
     <div className='game-info-container'>
       <span>
-        <FontAwesomeIcon icon={faStar} className='star' /> 0
+        <FontAwesomeIcon icon={faStar} className='star' /> {playerPoints}
       </span>
       <span>
-        <FontAwesomeIcon icon={faHeart} className='heart' /> 3
+        <FontAwesomeIcon icon={faHeart} className='heart' /> {playerLives}
       </span>
     </div>
   );
-}
+};
